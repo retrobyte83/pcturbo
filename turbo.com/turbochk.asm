3d35b97a69bdbbc8d817f5aa0854d79f42d30ec3
retrobyte83 <retrobyte83@gmail.com>
2023-11-30 19:36:37 +0000

    almost there!

--- turbo.com/turbochk.asm
+++ turbo.com/turbochk.asm
@@ -1,6 +1,5 @@
 		org 100h
 
-		xchg bx, bx
 		push	cs
 		pop	ds
 		jmp	loc_CAD 
@@ -19,7 +18,7 @@ word_178	dw 700h
 		db    6
 		db    7
 word_180	dw 300h			
-word_182	dw 0E000h		
+word_182	dw 0E000h	
 
 			db  "---stack base---ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
 stk_base	db 0
@@ -33,8 +32,6 @@ str_0A33h	db  "Unexpected Result",0Ah,0Dh,24h
 byte_C06	db 0	
 byte_C1E	db 0			
 str_0C1Fh	db    0
-			db    0
-			db    0
 			db    0
 			db    0
 			db    0
@@ -55,84 +52,16 @@ loc_CAD:
 		mov	ss, ax
 		mov	sp, stk_base
 		sti
-		cld
-		
-
-		jmp loc_1059 
-
-		mov	si, str_6D2
-		call	sub_1789 ; print Already executing on the PCturbo 286!
-		int	20h		; DOS -	PROGRAM	TERMINATION
-					; returns to DOS--identical to INT 21/AH=00h
-
-loc_1059:				
-		cli
-		in	al, 21h		; Interrupt controller,	8259A.
-		mov	byte [byte_C1E], al
-		push	ds
-		mov	si, 0
-		mov	ds, si
-		mov	di, str_0C1Fh
-		push	cs
-		pop	es
-
-		mov	cx, 4h
-		rep movsb
-		pop	ds
-		sti
-		cmp	byte [byte_CAB], 1
-		jz	short loc_10C4
-
-		mov	dx, cs:word_180
-		mov	al, 0FFh
-		out	dx, al
+		cld		
 	
-	
-	
-		mov	al, 0Fh
-		
-		mov	dx, cs:word_180
-		add	dl, 4
-		out	dx, al
-		mov	cs:byte_C06, al
-		
-
-
-		mov	es, [word_182]
-
-		mov	si, str_80Bh  ; PCturbo 286
-		mov	di, 0E00Ah
-		mov	cx, 0Bh
-		repe cmpsb
-		jnz	short loc_10C4
-		mov	cx, 64h
-		mov	byte  es:0FFFDh, 0A5h
-
-loc_109D:				
-		cmp	byte es:0FFFDh, 5Ah
-		loopne	loc_109D
-		jnz	short loc_10C4
-		mov	byte es:0FFFDh, 0F0h
-
-loc_10AD:				
-		cmp	byte es:0FFFDh, 0Fh
-		loopne	loc_10AD
-		jnz	short loc_10C4
-		mov	byte [byte_CA9], 1
-		or	byte [byte_CA7], 80h
-		jmp	loc_12D9
-
-loc_10C4:				
-		mov	dx, cs:word_180
-		add	dl, 4
-		mov	al, 0
-		out	dx, al
-		mov	dx, cs:word_180
-		add	dl, 3
-		mov	al, cs:byte_CA7
-		out	dx, al
-	
-		
+		mov	ax, [word_182]
+		mov	cl, 4
+		shr	ah, cl
+		sub	ah, 8
+		or	byte [byte_CA7], ah
+		or	byte [byte_CA7], 10h
+				
+		call	sub_183B
 		mov	dx, [word_178]
 		add	dl, 7
 		out	dx, al
@@ -146,21 +75,9 @@ loc_10C4:
 		add	dl, 1
 		mov	al, 0
 		out	dx, al
-
-		mov	dx, cs:word_180
-		mov	al, 0FFh
-		out	dx, al
-
-
+		call	sub_1828
 		mov	al, 0Fh
-
-
-		mov	dx, cs:word_180
-		add	dl, 4
-		out	dx, al
-		mov	cs:byte_C06, al		
-		
-
+		call	sub_1854
 		mov	es, [word_182]
 		mov	word es:0, 5AA5h
 		mov	word es:2, 0A55Ah
@@ -209,6 +126,7 @@ loc_1790:
 		inc	si
 		jmp	short loc_1790
 
+
 loc_179E:
 		pop	bp
 		pop	si
@@ -216,6 +134,35 @@ loc_179E:
 		pop	ax
 		retn
 ;sub_1789	endp
+
+; ���������������������������������������������������������������������������
+
+sub_1828:
+		mov	dx, cs:word_180
+		mov	al, 0FFh
+		out	dx, al
+		retn
+;sub_1828	endp
+
+sub_183B:
+		mov	dx, cs:word_180
+		add	dl, 4
+		mov	al, 0
+		out	dx, al
+		mov	dx, cs:word_180
+		add	dl, 3
+		mov	al, cs:byte_CA7
+		out	dx, al
+		retn
+;sub_183B	endp
+
+sub_1854:
+		mov	dx, cs:word_180
+		add	dl, 4
+		out	dx, al
+		mov	cs:byte_C06, al
+		retn
+;sub_1854	endp
 
 
 byte_2123	db 9	
